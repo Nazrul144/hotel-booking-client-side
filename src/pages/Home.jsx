faafb0d184e4e3e56cf61485aeee1a3773f7bb52
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-05-10 17:56:46 +0600

    tailwind and daisy ui added in the project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='font-bold text-6xl text-center mt-56 font-naz '>This is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
